5a4b34f3369958a8321c47b4906e00e44a8b97d7
Ceoael <hedreder@gmail.com>
2021-05-17 19:08:58 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <countdown-timer-builder></countdown-timer-builder>
+</template>
+
+<script>
+  import CountdownTimerBuilder from './pages/CountdownTimerBuilder.vue';
+
+  export default {
+    components : {
+      CountdownTimerBuilder
+    }
+  }
+</script>
+
+<style>
+  * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+  body {
+    background-color: #100E1D;
+  }
+</style>
